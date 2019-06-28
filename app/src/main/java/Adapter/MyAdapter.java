93123ec650924f522c5dc6f8a0a3fa0c7f92b88b
Ken <kensong96@gmail.com>
2019-06-28 15:47:59 -0400

    98D

--- app/src/main/java/Adapter/MyAdapter.java
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -1,6 +1,7 @@
 package Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.z52song.recyclerviews.DetailActivity;
 import com.example.z52song.recyclerviews.R;
 
 import java.util.List;
@@ -70,7 +72,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
             ListItem item = listItems.get(position);
 
-            Toast.makeText(context, item.getName(), Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(context, DetailActivity.class);
+
+            intent.putExtra("name", item.getName());
+            intent.putExtra("description", item.getDescription());
+            intent.putExtra("rating", item.getRating());
+
+            context.startActivity(intent);
+
+            //Toast.makeText(context, item.getName(), Toast.LENGTH_SHORT).show();
         }
     }
 }
